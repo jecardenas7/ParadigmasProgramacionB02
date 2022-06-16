81790b9c4d3960509ee9bd8f2f1f01cc75894396
jecardenas7 <106890295+jecardenas7@users.noreply.github.com>
2022-06-16 08:02:05 -0500

    Add files via upload

---
+++ Tienda_Ana/src/tienda_ana/Tienda_Ana.java
@@ -0,0 +1,16 @@
+package tienda_ana;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Tienda_Ana extends Application {
+public static void main(String[] args) {
+   
+}
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
