de0a346e79ea15fcf7652504fd2b95c5cd2997d3
jecardenas7 <106890295+jecardenas7@users.noreply.github.com>
2022-06-28 22:06:04 -0500

    Add files via upload

---
+++ TiendaDoñaAna/src/application/MainSceneController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.event.ActionEvent;
+
+public class MainSceneController {
+	@FXML private TextArea Bienvenido;
+	
+	@FXML private void accionSiguienteAnchor (ActionEvent event) {
+		
+
+	}
+}
